a4cac17513075b636cfe18bce7cd6f8b139d1d71
Najmul890 <najmul890890@gmail.com>
2024-02-08 01:50:59 +0600

    todo context created for wrap the app to provide the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { useReducer } from "react";
 import ActionsContainer from "./components/ActionsContainer/ActionsContainer";
 import MobileBottomActions from "./components/ActionsContainer/MobileBottomActions";
 import TodoListContainer from "./components/TodoListContainer/TodoListContainer";
+import { initialState, todoReducer } from "./reducers/TodoReducer";
+import { TodoContext } from "./context";
 
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
   return (
-    <div className=" h-screen flex justify-center items-center ">
-      <div className=" bg-white shadow-lg w-full sm:w-[96%] xl:w-3/4 rounded-lg py-10 px-6 xl:px-10 m-auto ">
-        <ActionsContainer />
-        <TodoListContainer />
+    <TodoContext.Provider value={{ state, dispatch }}>
+      <div className=" h-screen flex justify-center items-center ">
+        <div className=" bg-white h-screen sm:h-auto shadow-lg w-full sm:w-[96%] xl:w-3/4 rounded-lg py-10 px-6 xl:px-10 m-auto ">
+          <ActionsContainer />
+          <TodoListContainer />
+        </div>
+        <MobileBottomActions />
       </div>
-      <MobileBottomActions/>
-    </div>
+    </TodoContext.Provider>
   );
 }
 
