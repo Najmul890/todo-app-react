85b3bc964e8bd978d81ff0b71ed3dc7a205881d2
Najmul890 <najmul890890@gmail.com>
2024-02-09 12:10:54 +0600

    few more comments added while filtering todos in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,22 @@ function App() {
   const [statusFilterValue, setStatusFilterValue] = useState("");
   const [searchParams, setSearchParams] = useState("");
 
+  // todos of initial state is set as filtered todos initially
   let filteredTodos = [...state.todos];
 
+  // filtering todos via search params
   filteredTodos = state.todos.filter((todo) =>
     todo.title.toLowerCase().includes(searchParams.toLowerCase())
   );
 
+  // filtering todos via todo priority
   if (priorityFilterValue) {
     filteredTodos = filteredTodos.filter(
       (todo) => todo.priority === priorityFilterValue
     );
   }
 
+  // filtering todos via todo status
   if (statusFilterValue) {
     filteredTodos = filteredTodos.filter(
       (todo) => todo.status === statusFilterValue
@@ -52,7 +56,7 @@ function App() {
 
   return (
     <TodoContext.Provider value={value}>
-      <div className=" min-h-screen py-5 flex justify-center items-center ">
+      <div className=" min-h-screen py-0 sm:py-5 flex justify-center items-center ">
         <div className=" bg-white min-h-screen sm:min-h-[500px] shadow-lg w-full sm:w-[96%] xl:w-3/4 rounded-lg py-10 px-6 xl:px-10 m-auto ">
           <ActionsContainer />
           <TodoListContainer />
